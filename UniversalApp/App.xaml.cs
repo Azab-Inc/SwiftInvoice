920c3ee8c8acc398b82d3ba39444b0569959baa4
therealcoolpup <azcooldude8@gmail.com>
2023-06-24 20:35:37 +0200

    Base project

---
+++ UniversalApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace UniversalApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ UniversalApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversalApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
