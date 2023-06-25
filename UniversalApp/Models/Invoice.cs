eee01d05745364a9bd924c1f648bd69897ce035b
therealcoolpup <azcooldude8@gmail.com>
2023-06-25 23:24:02 +0200

    Models

---
+++ UniversalApp/Models/Invoice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalApp.Models
+{
+    internal class Invoice
+    {
+        public int InvoiceId { get; set; }
+
+        public string ClientName { get; set; }
+
+        public int BusinessNumber { get; set; }
+
+        public string WebsiteLink { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Total { get; set; }
+
+        public int[] ItemIds { get; set; }
+
+
+    }
+}
---
+++ UniversalApp/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversalApp.Models
+{
+    internal class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int InvoiceId { get; set; }
+    }
+}
