54d65c2f61a8a20c4430c0994a1e7c560868854b
therealcoolpup <azcooldude8@gmail.com>
2023-06-26 02:35:20 +0200

    Create User.cs

---
+++ UniversalApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UniversalApp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string BusinessNumber { get; set; }
+    }
+}
