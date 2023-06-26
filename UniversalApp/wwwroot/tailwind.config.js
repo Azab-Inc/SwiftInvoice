6c719e37c4ef39956cdbe09a10769b5e5e8b65ce
therealcoolpup <azcooldude8@gmail.com>
2023-06-26 02:35:02 +0200

    Added colours to tailwind config

--- UniversalApp/wwwroot/tailwind.config.js
+++ UniversalApp/wwwroot/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     content: ["**/**"],
     theme: {
         extend: {
-
+            colors: {
+                'pri': '#3775f8',
+                'warn': '#ffc107',
+                'danger': '#dc3545',
+                'accent': "#4f5b6c"
+            }
         },
     },
     plugins: [],
