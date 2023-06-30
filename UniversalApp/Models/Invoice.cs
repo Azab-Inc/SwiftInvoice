54a4be79bbfd779ad0e891ef3f1cdf4dffda5b4c
therealcoolpup <azcooldude8@gmail.com>
2023-06-30 13:21:58 +0200

    Added more fields to invoice

--- UniversalApp/Models/Invoice.cs
+++ UniversalApp/Models/Invoice.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UniversalApp.Models
+﻿namespace UniversalApp.Models
 {
     public class Invoice
     {
@@ -12,11 +6,15 @@ namespace UniversalApp.Models
 
         public string ClientName { get; set; }
 
-        public int BusinessNumber { get; set; }
+        public string JobName { get; set; } 
 
-        public string Website { get; set; }
+        public string JobDescription { get; set; }
 
-        public DateTime Date { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime PaymentDate { get; set; }
 
         public decimal Total { get; set; }
 
