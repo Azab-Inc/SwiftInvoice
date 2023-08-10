4b4d8bf1bc3c649684a6d085867cb5f2a06d90ce
AZ <47402666+realazaber@users.noreply.github.com>
2023-08-10 15:14:51 +0200

    Converted types to avoid to big int error

--- UniversalApp/Models/User.cs
+++ UniversalApp/Models/User.cs
@@ -12,14 +12,17 @@ namespace UniversalApp.Models
         public string AccString { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int BusinessNumber { get; set; }
+        public string BusinessNumber { get; set; }
 
         public string BusinessName { get; set; }
         public string Email { get; set; }
         public string BankAccNumber { get; set; }
+        public string BranchIdentifier { get; set; }
         public string BankName { get; set; }
+
+        public string Currency { get; set; }
         public string LogoPath { get; set; }
-        public int Phone { get; set; }
+        public string Phone { get; set; }
         public string Website { get; set; }
     }
 }
