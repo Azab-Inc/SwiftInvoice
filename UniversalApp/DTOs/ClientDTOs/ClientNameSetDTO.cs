99cfa6cf55363d852a522bce105dd9e1eb209b00
AZ <47402666+realazaber@users.noreply.github.com>
2024-12-09 03:19:25 +1100

    Feature: Client dtos for table view and for dropdown

---
+++ UniversalApp/DTOs/ClientDTOs/ClientNameSetDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversalApp.DTOs
+{
+    public record ClientNameSetDTO(
+        int Id,
+        string FirstName,
+        string LastName,
+        string? BusinessName,
+        bool isB2B);
+}
---
+++ UniversalApp/DTOs/ClientDTOs/ClientPreviewDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace UniversalApp.DTOs.ClientDTOs
+{
+    public record ClientPreviewDTO(
+    int Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Phone,
+    bool isB2B,
+    string ProfileImg,
+    int Invoices
+        );
+
+}
