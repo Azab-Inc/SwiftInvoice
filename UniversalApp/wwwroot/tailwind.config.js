4cd00ff75afd8d8f7906cd946ec513081b6b2b4e
AZ <47402666+realazaber@users.noreply.github.com>
2024-12-09 03:11:26 +1100

    Package: Installed Daisy UI

--- UniversalApp/wwwroot/tailwind.config.js
+++ UniversalApp/wwwroot/tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["**/**"],
-    theme: {
-        extend: {
-            colors: {
-                'pri': '#1478aa',
-                'sec': '#03a9f4',
-                'success': '#28a745',
-                'warn': '#ffc107',
-                'danger': '#dc3545',
-                'accent': "#4f5b6c"
-            }
-        },
+  content: ["**/**"],
+  theme: {
+    extend: {
+      colors: {
+        pri: "#1478aa",
+        sec: "#03a9f4",
+        success: "#28a745",
+        warn: "#ffc107",
+        danger: "#dc3545",
+        accent: "#4f5b6c",
+      },
     },
-    plugins: [],
-}
+  },
+  daisyui: {
+    themes: false,
+  },
+  plugins: [require("daisyui")],
+};
