71dbe39e640bd88ab6222c145300ca4267652629
AZ <47402666+realazaber@users.noreply.github.com>
2024-12-10 03:55:07 +1100

    Feature: SKUs

---
+++ UniversalApp/Models/SKU.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversalApp.Models
+{
+    [Table("skus")]
+    public class SKU : IHasUserId
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Name")]
+        public string SKUName { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ UniversalApp/Services/SKUService.cs
@@ -0,0 +1,44 @@
+﻿using UniversalApp.DTOs;
+using UniversalApp.Interfaces;
+using UniversalApp.Models;
+
+namespace UniversalApp.Services
+{
+    public class SKUService : GenericService<SKU, SKUTableDTO>, ISearchable<SKU>, IPreventDuplicate<SKU>
+    {
+
+        private InvoiceService invoiceService = new InvoiceService();
+        public bool alreadyExists(int userId, string search)
+        {
+            search = search.ToLower();
+            using (var connection = dbService.GetConnection()) { 
+                return connection.Table<SKU>()
+                                 .Where(s => s.SKUName.ToLower() == search.ToLower())
+                                 .Any();
+                
+            }
+        }
+
+        public List<SKU> dbSearch(int userId, string search)
+        {
+            using (var connection = dbService.GetConnection()) {
+                search = search.ToLower();
+                return connection.Table<SKU>()
+                    .Where(c => c.UserId == userId)
+                    .Where(c => c.SKUName.ToLower().Contains(search))                    
+                    .ToList();
+            }
+        }
+
+        public int newestNum(int userId, int skuId)
+        {
+            using (var connection = dbService.GetConnection())
+            {
+                int linkedInvoices = invoiceService.dbGetNumSkuInvoices(userId, skuId);
+                return linkedInvoices;
+            }
+
+            
+        }
+    }
+}
